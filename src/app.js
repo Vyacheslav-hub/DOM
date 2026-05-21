5a1b5b1aeea671dee97c14b2f89b43299a66aafa
Vyacheslav <slavik.kurganov.26@mail.ru>
2026-05-21 18:42:51 +0300

    для теста изменил файлы

--- src/app.js
+++ src/app.js
@@ -2,3 +2,11 @@ import "./css/index.css";
 
 const a = 2;
 console.log(a)
+
+
+const gg = document.querySelector('#game-board');
+
+const bb = document.createElement('div');
+bb.classList.add('b')
+
+gg.append(bb);
