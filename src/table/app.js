4b0c1c3df79e1d3f204cc25c117aaa658ea60489
Vyacheslav <slavik.kurganov.26@mail.ru>
2026-05-22 17:46:59 +0300

    Добавил файлы для второго задания

---
+++ src/table/app.js
@@ -0,0 +1,9 @@
+import "../goblins-game/style.css";
+import GoblinsGame from "../goblins-game/GoblinsGame.js";
+
+document.addEventListener('DOMContentLoaded', () => {
+    const game = new GoblinsGame();
+
+    game.init();
+
+});
