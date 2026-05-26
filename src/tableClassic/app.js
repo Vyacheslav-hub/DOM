75211cbecabd8a6f905609358999768422adcdd6
Vyacheslav <slavik.kurganov.26@mail.ru>
2026-05-26 17:13:59 +0300

    Подключил к одному файлу

---
+++ src/tableClassic/app.js
@@ -0,0 +1,6 @@
+import "../goblins-game/style.css";
+import "../table/style.css"
+import initTableClassic from "./tableClassic.js";
+document.addEventListener('DOMContentLoaded', () => {
+    initTableClassic()
+})
